9bb970393725d9a5f6577803271a408ac39cdadf
mathtijs <matthijsjurriaans333@gmail.com>
2021-11-04 12:21:37 +0100

    dio brando

--- dio brando stats.py
+++ dio brando stats.py
@@ -1,6 +1,15 @@
-print ("name: dio brando")
-print ("stand: the world")
-print ("age: 100")
-print ("stand level: 100")
-print ("stand ability: stop time")
-print ("weakness: UV light")
+name = "dio brando"
+stand = "the world"
+age = 100
+print (age)
+standlevel = "100"
+standability = "stop time"
+weakness = "UV light"
+hasCorona = False
+
+print ("name:"+name)
+print ("stand:"+stand)
+print ("stand level:"+standlevel)
+print ("stand ability:"+standability)
+print ("weakness:"+weakness)
+print ("has corona"+str(hasCorona))
